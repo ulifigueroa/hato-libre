6d6d78f9ecf741b846a0710fde4f3c15d74376b0
Ulises Figueroa <Ulises@Ulisess-MacBook-Pro-3.local>
2016-07-11 22:11:11 -0500

    Making index a static page.

--- index.php
+++ index.php
@@ -1,31 +1,18 @@
 <?php get_header(); ?>
 
 <main class="site-main" role="main">
-  <section class="index-banner banner">
-    <div class="content">
-      <h1>Soluciones tecnológicas para empresas ganaderas</h1>
-      <p>HATO LIBRE® es una alternativa para satisfacer la necesidad de
-        servicios de rastreabilidad, diagnóstico, monitoreo pasivo y control
-        sanitario de las empresas ganaderas</p>
-      <a href="#">Sobre Nosotros</a>
-    </div>
-  </section>
+  <?php
 
-  <section class="index-banners clear">
-    <div class="content">
-      <article class="services">
-        <h1>Servicios</h1>
-        <p>Rastreabilidad de ganado, Laboratorio, CRO.</p>
-        <a href="#">Saber mas</a>
-      </article>
+  while ( have_posts() ) : the_post();
 
-      <article class="partners">
-        <h1>Alianzas</h1>
-        <p>Los aliados estratégicos son una poderosa fuente de clientes referidos y calificados.</p>
-        <a href="#">Saber mas</a>
-      </article>
-    </div>
-  </section>
+    get_template_part( 'content', 'page' );
+
+    if ( comments_open() || get_comments_number() ) :
+      comments_template();
+    endif;
+
+  endwhile;
+  ?>
 </main>
 
 <?php get_footer(); ?>
